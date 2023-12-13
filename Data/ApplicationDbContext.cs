2affb57317f4531ed03b7b128759c56413806c6f
Ajay Yadav <ajayyadav12170@gmail.com>
2023-12-13 07:56:41 +0530

    initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcTutorial.Models;
+
+namespace MvcTutorial.Data
+{
+    public class ApplicationDbContext : DbContext //download DbContext from package Manager : Microsoft.EntityFrameworkCore
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcTutorial.Models
+{
+    [Table("Category")]
+    public class Category
+    {
+        //[Key]
+        public int Id { get; set; }
+        [Required]
+
+        public string Name { get; set; }
+
+        [DisplayName( "Order")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+    }
+}
