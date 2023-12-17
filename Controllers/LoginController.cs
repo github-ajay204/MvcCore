031635ffafd9a76d66a5d19528729e1bc0292772
Ajay Yadav <ajayyadav12170@gmail.com>
2023-12-17 16:42:26 +0530

    Crud done

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using MvcTutorial.Models;
+using System.Data;
+using System.Configuration;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace MvcTutorial.Controllers
+{
+    public class LoginController : Controller
+    {
+        #region Connection string
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        #endregion
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public  IActionResult AuthUser(VMLogin ModelLogin)
+        {
+            bool Isvalid= false;
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                using (SqlCommand command = new SqlCommand("PRC_USER_IUser", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Mailid", ModelLogin.MailId);
+                    command.Parameters.AddWithValue("@Password", ModelLogin.Password);
+                    connection.Open();
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        if (dataReader.Read())
+                        {
+                            Isvalid = true;
+                        }
+                        else
+                        {
+                            return NotFound();
+                        }
+                    }
+                }
+            }
+            if(Isvalid == true)
+            {
+                return RedirectToAction("Index", "Demo");
+            }
+            return RedirectToAction("NotFound", "Error");
+        }
+    }
+}
---
+++ Models/VMLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcTutorial.Models
+{
+    public class VMLogin
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string MailId { get; set; }
+    }
+}
